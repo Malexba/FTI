a43e0e1bdd243b850f91e1e15f81dc31a40fec22
Malexba <80394779+Malexba@users.noreply.github.com>
2021-03-16 11:05:42 +0100

    Add files via upload

---
+++ PRACTICA XML/validador/src/validador/Validador.java
@@ -0,0 +1,39 @@
+package validador;
+
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Validador {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        domFactory.setValidating(true);
+        DocumentBuilder builder = domFactory.newDocumentBuilder();
+        builder.setErrorHandler(new ErrorHandler() {
+            @Override
+            public void error(SAXParseException exception) throws SAXException {
+                // do something more useful in each of these handlers
+                exception.printStackTrace();
+            }
+            @Override
+            public void fatalError(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+            }
+
+            @Override
+            public void warning(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+            }
+        });
+        Document doc = builder.parse("recetario.xml");
+    }
+}
